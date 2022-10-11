252bd741424a0f9d6b4dfb38e9cbef21c2852350
muthuaravind-test <81284530+muthuaravind-test@users.noreply.github.com>
2022-10-11 22:08:06 +0530

    Add files via upload

--- Java.js
+++ Java.js
@@ -1,19 +1,19 @@
-$(document).ready(function(){
-    $(".c1").keyup(function(){
-        var LoanAmount = parseInt(document.getElementById('LA').value);
-        var Annual_Interest_Rate = parseInt(document.getElementById('AIR').value);
-        var Tenure = parseInt(document.getElementById('T').value);
-        var P = LoanAmount;
-        var R = (Annual_Interest_Rate/12)/100;
-        var N = Tenure*12;
-        var EMI = P * R * ((1+R)**N)/(((1+R)**N) - 1)
-        var TR = EMI*12*Tenure;
-        var TotalInterest = TR - P;
-        
-        $("#o1").html("Loan Amount: Rs."+Math.ceil(LoanAmount));
-        $("#o2").html("Total Interest: Rs."+Math.ceil(TotalInterest));
-        $("#o3").html("EMI: Rs."+Math.ceil(EMI));
-        $("#o4").html("Total Repayment: Rs."+Math.ceil(TR));
+function fun() {
+    
+    var LoanAmount = parseInt(document.getElementById('LA').value);
+    var Annual_Interest_Rate = parseInt(document.getElementById('AIR').value);
+    var Tenure = parseInt(document.getElementById('T').value);
 
-    })
-})
+    var P = LoanAmount;
+    var R = (Annual_Interest_Rate/12)/100;
+    var N = Tenure*12;
+
+    var EMI = P * R * ((1+R)**N)/(((1+R)**N) - 1)
+    var TR = EMI*12*Tenure;
+    var TotalInterest = TR - P;
+
+    document.getElementById('o1').innerHTML = "Loan Amount: ₹"+ Math.ceil(LoanAmount);
+    document.getElementById('o2').innerHTML = "Total Interest: ₹"+ Math.ceil(TotalInterest);
+    document.getElementById('o3').innerHTML = "EMI: ₹" + Math.floor(EMI);
+    document.getElementById('o4').innerHTML = "Total Repayment: ₹" + Math.ceil(TR);
+}
